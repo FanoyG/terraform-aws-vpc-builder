6ccd787b40d139eeb1b5ff04d923c09ab4c9a199
Adil Khan <ismartk13@gmail.com>
2025-06-24 17:27:51 +0530

    Handle User edge case for group_policy_arns

--- modules/iam/variables.tf
+++ modules/iam/variables.tf
@@ -1,11 +1,23 @@
 variable "group_name" {
   description = "IAM Group Name"
   type        = string
+
+  validation {
+    condition = length(var.group_name) > 3 && length(var.group_name) < 64
+    error_message = "Group name must be between 4 and 63 characters."
+  }
 }
 
 variable "user_names" {
   description = "List of IAM User names"
   type        = list(string)
+
+  validation {
+    condition = alltrue([
+        for name in var.user_names : can(regex("^[a-zA-Z][a-zA-Z0-9]{3,63}$", name))
+    ])
+    error_message = "Each IAM username must be 1-64 characters and match IAM naming rules."
+  }
 
 }
 
@@ -18,4 +30,12 @@ variable "group_policy_arns" {
   type        = list(string)
   default     = []
   description = "List of IAM managed policy ARNs to attach to the group"
+
+
+    validation {
+    condition = alltrue([
+      for arn in var.group_policy_arns : can(regex("^arn:aws:iam::aws:policy/.+", arn))
+    ])
+    error_message = "Each policy ARN must be a valid managed policy under aws:policy."
+  }
 }

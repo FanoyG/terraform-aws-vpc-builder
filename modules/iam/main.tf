a1898d598283525bc44418a1dd8d28119b26d2e8
Adil Khan <ismartk13@gmail.com>
2025-06-24 17:08:22 +0530

    Add a Resource group_policy_attachement

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -4,14 +4,20 @@ resource "aws_iam_group" "this" {
 
 resource "aws_iam_user" "this" {
   for_each = toset(var.user_names)
-  name = each.value
+  name     = each.value
 
   tags = var.default_tags
 }
 
 
 resource "aws_iam_user_group_membership" "membership" {
-    for_each = toset(var.user_names)
-    user = each.value
-    groups = [aws_iam_group.this.name]
+  for_each = toset(var.user_names)
+  user     = each.value
+  groups   = [aws_iam_group.this.name]
+}
+
+resource "aws_iam_group_policy_attachment" "group_policies" {
+  count      = length(var.group_policy_arns)
+  group      = aws_iam_group.this.name
+  policy_arn = var.group_policy_arns[count.index]
 }

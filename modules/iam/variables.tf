d5bb081ecf71a495b54a76966035e713368cec42
Adil Khan <ismartk13@gmail.com>
2025-06-24 17:09:02 +0530

    Add a group_policy_arn

--- modules/iam/variables.tf
+++ modules/iam/variables.tf
@@ -1,15 +1,21 @@
 variable "group_name" {
   description = "IAM Group Name"
-  type = string
+  type        = string
 }
 
 variable "user_names" {
-    description = "List of IAM User names"
-    type = list(string)
-  
+  description = "List of IAM User names"
+  type        = list(string)
+
 }
 
 variable "default_tags" {
-    type =  map(string)
-    default = {}
+  type    = map(string)
+  default = {}
+}
+
+variable "group_policy_arns" {
+  type        = list(string)
+  default     = []
+  description = "List of IAM managed policy ARNs to attach to the group"
 }

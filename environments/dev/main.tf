0980f670e4419c940445cf409260e8e78909e8e9
Adil Khan <ismartk13@gmail.com>
2025-06-24 13:20:00 +0530

    chore(env): setup dev environment with AWS provider and VPC module call

---
+++ environments/dev/main.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+  shared_config_files      = ["~/.aws/config"]
+  shared_credentials_files = ["~/.aws/credentials"]
+  profile                  = "FanoyG98"
+  region                   = "ap-south-1"
+}
+
+module "vpc" {
+  source          = "../../modules/vpc"
+  vpc_cidr        = "10.0.0.0/16"
+  project_name    = "cloudforge"
+
+
+  availability_zones = ["ap-south-1a", "ap-south-1b"]
+  public_subnets = ["10.0.1.0/24",     "10.0.3.0/24"]
+  private_subnets = ["10.0.101.0/24",  "10.0.102.0/24"]
+
+
+  default_tags    = {
+    Owner     = "Fanoy"
+    Project   = "VPC-Builder"
+    ManagedBy = "Terraform"
+    Environment = "dev"
+  }
+
+}
